2ead2f3a41d35a80f464d7c04f47ec611bfc296f
Shystra <jlsp.2019@hotmail.com>
2024-02-16 19:47:19 -0300

    feat: add new library with use shadcn

--- gifts-list/src/app/(pages)/(home)/page.tsx
+++ gifts-list/src/app/(pages)/(home)/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Header  from '../../components/Header'
 
 export default function Home() {
   return (
     <>
-      <h1>teste</h1>
+      <Header />
+
     </>
   );
 }
---
+++ gifts-list/src/app/components/Header.tsx
@@ -0,0 +1,13 @@
+import { Card, CardContent } from "./ui/card"
+
+const Header = () => {
+    return (
+        <Card>
+            <CardContent className="p-5 bg-slate-600">
+                
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Header 
