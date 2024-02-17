01904d2ac036e75514655d2518bfcb7c221b4efa
Shystra <jlsp.2019@hotmail.com>
2024-02-16 21:27:10 -0300

    just button

--- gifts-list/src/app/components/Header.tsx
+++ gifts-list/src/app/components/Header.tsx
@@ -1,10 +1,12 @@
+import Image from "next/image"
 import { Card, CardContent } from "./ui/card"
 
 const Header = () => {
     return (
         <Card>
-            <CardContent className="p-5 bg-slate-600">
-                
+            <CardContent className="p-5 py-8 justify-between items-center flex flex-row bg-slate-600">
+                <Image src="next.svg" alt="logo" height={22} width={120}/>
+                <Button></Button>
             </CardContent>
         </Card>
     )
